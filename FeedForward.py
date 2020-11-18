5afbb5b302721d37c14eb9bb74793206ae7653b1
mahdi akhi <mahdiAkhy@gmail.com>
2020-11-18 22:38:41 +0330

    Create Neural Network Model in Feedforward Class

---
+++ FeedForward.py
@@ -0,0 +1,38 @@
+from numpy import array, dot, tanh, exp, random
+
+
+class Feedforward:
+    def __init(self):
+
+        # generate same weight in every run
+        random.seed(1)
+
+        # 3x1 matrix weight
+        self.weight_matrix = 2 * random.random((3, 1)) - 1
+
+    # tanh as activation function
+    def tanh(self, x):
+        return tanh(x)
+
+    # derivative of tanh function
+    # needed to calculate the gradients
+    def tanh_derivitave(self, x):
+        return 1.0 - tanh(x) ** 2
+
+    # foreard propagataion
+    def forward_propagation(self, inputs):
+        return self.tanh(dot(inputs, self.weight_matrix))
+
+    def train(self, train_inputs, train_outputs, num_train_itterations):
+        for i in range(num_train_itterations):
+            output = self.forward_propagation(train_inputs)
+
+            # calculate error in output
+            error = train_outputs - output
+
+            adjustment = dot(train_inputs.T - error *
+                             self.tanh_derivitave(output))
+
+            # adjust the weight matrix
+
+            self.weight_matrix += adjustment
